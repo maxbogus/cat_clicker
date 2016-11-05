c92ff156a7263023763b3db8d97d80572f3b3968
Max Boguslavskiy <max.bogus@gmail.com>
2016-11-05 04:03:24 +0300

    * created list of objects; * added reference code to project; * commented it out.

--- js/code.js
+++ js/code.js
@@ -1,13 +1,25 @@
-var cat_1 = {
+var cats = [{
     'name': 'Alisa',
     'picture': 'img/cat_1.jpg',
     'clicks': '0'
-};
-var cat_2 = {
+}, {
     'name': 'Boris',
-    'picture': 'img/cat_1.jpg',
+    'picture': 'img/cat_2.jpg',
     'clicks': '0'
-};
+}, {
+    'name': 'Marusya and Teresa',
+    'picture': 'img/cat_3.jpg',
+    'clicks': '0'
+}, {
+    'name': 'Ball',
+    'picture': 'img/cat_4.jpg',
+    'clicks': '0'
+}, {
+    'name': 'Phillip',
+    'picture': 'img/cat_5.jpg',
+    'clicks': '0'
+}];
+
 $('#cat_image_1').click(function (e) {
     var number = $('#number_1').text();
     console.log(number);
@@ -18,3 +30,28 @@ $('#cat_image_2').click(function (e) {
     console.log(number);
     $('#number_2').text(parseInt(number) + 1);
 });
+
+// // clear the screen for testing
+// document.body.innerHTML = '';
+//
+// var nums = [1,2,3];
+//
+// // Let's loop over the numbers in our array
+// for (var i = 0; i < nums.length; i++) {
+//
+//     // This is the number we're on...
+//     var num = nums[i];
+//
+//     // We're creating a DOM element for the number
+//     var elem = document.createElement('div');
+//     elem.textContent = num;
+//
+//     // ... and when we click, alert the value of `num`
+//     elem.addEventListener('click', (function(numCopy) {
+//         return function() {
+//             alert(numCopy);
+//         };
+//     })(num));
+//
+//     document.body.appendChild(elem);
+// };
