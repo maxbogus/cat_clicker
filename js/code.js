c365d4e6b9ebaad0a4bed4d210ca7e44f873e4c3
Max Boguslavskiy <max.bogus@gmail.com>
2016-11-05 03:50:46 +0300

    + added second cat picture; * updated html structure; + added code to store second cat.

--- js/code.js
+++ js/code.js
@@ -1,5 +1,20 @@
-$('#cat_image').click(function (e) {
-    var number = $('#number').text();
+var cat_1 = {
+    'name': 'Alisa',
+    'picture': 'img/cat_1.jpg',
+    'clicks': '0'
+};
+var cat_2 = {
+    'name': 'Boris',
+    'picture': 'img/cat_1.jpg',
+    'clicks': '0'
+};
+$('#cat_image_1').click(function (e) {
+    var number = $('#number_1').text();
     console.log(number);
-    $('#number').text(parseInt(number) + 1);
+    $('#number_1').text(parseInt(number) + 1);
+});
+$('#cat_image_2').click(function (e) {
+    var number = $('#number_2').text();
+    console.log(number);
+    $('#number_2').text(parseInt(number) + 1);
 });
