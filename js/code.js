78254c72a68319906a476e1d42a07e7e0f099500
Max Boguslavskiy <max.bogus@gmail.com>
2016-11-14 02:29:49 +0300

    + added todo.

--- js/code.js
+++ js/code.js
@@ -58,3 +58,25 @@ $('div#active').click(function (e) {
     console.log(current['clicks']);
     $('span.active').text(current['clicks']);
 });
+
+//TODO:
+// 1. separate model and view:
+// a. model - cats var
+// b. view - controls and clicks, render
+// c. octopus - init, interaction between view and model
+
+// Project Requirements for Cat Clicker Premium
+// Visuals
+//The application should display
+// a list of cats by name
+// an area to display the selected cat
+
+// In the cat display area, the following should be displayed
+// the cat's name
+// a picture of the cat
+// text showing the number of clicks
+
+// The specifics of the layout do not matter, so style it however you'd like.
+// Interaction
+// When a cat name is clicked in the list, the cat display area should update to show the data for the selected cat.
+//    The number of clicks in the cat area should be unique to each cat, and should increment when the cat's picture is clicked.
