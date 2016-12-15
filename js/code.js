67376e8c25f6a8382f859b244231b922742d1552
Max Boguslavskiy <max.bogus@gmail.com>
2016-12-16 02:27:50 +0300

    * separated viewModel and Model.

--- js/code.js
+++ js/code.js
@@ -1,17 +1,39 @@
-var ViewModel = function () {
+var Cat = function () {
     this.nickNames = ko.observableArray(['T-Bone', 'Fluffy', 'Warrior']);
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/cat_1.jpg');
     this.imgAttribution = ko.observable('none');
 
-    this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
-    };
+    this.title = ko.pureComputed(function () {
+        var title;
+        var clicks;
 
-    this.currentLevel = ko.pureComputed(function () {
-        return this.clickCount() > 30 ? "Newborn" : "Infant";
+        clicks = this.clickCount();
+
+        if (clicks < 10) {
+            title = "Newborn";
+        } else if (clicks < 50) {
+            title = "Infant";
+        } else if (clicks < 100) {
+            title = "Child";
+        } else if (clicks < 200) {
+            title = "Teen";
+        } else if (clicks < 500) {
+            title = "Adult";
+        } else {
+            title = "Ninja";
+        }
+
+        return title;
     }, this);
+};
+
+var ViewModel = function () {
+    this.currentCat = ko.observable(new Cat());
+    this.incrementCounter = function () {
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
 };
 
 ko.applyBindings(new ViewModel());
