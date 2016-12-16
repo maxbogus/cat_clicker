5479eb7bbb7811a2d5cd5d95427f4d69a15689c5
Max Boguslavskiy <max.bogus@gmail.com>
2016-12-16 03:18:51 +0300

    + added initialCats; * refactored current cat to get it from object.

--- js/code.js
+++ js/code.js
@@ -1,9 +1,48 @@
-var Cat = function () {
-    this.nickNames = ko.observableArray(['T-Bone', 'Fluffy', 'Warrior']);
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/cat_1.jpg');
-    this.imgAttribution = ko.observable('none');
+var initialCats = [
+    {
+        clickCount: 0,
+        name: "Alisa",
+        imgSrc: "img/cat_1.jpg",
+        imgAttribution: "none",
+        nicknames: ['T-Bone', 'Fluffy', 'Warrior']
+    }, {
+        clickCount: 0,
+        name: "Boris",
+        imgSrc: "img/cat_2.jpg",
+        imgAttribution: "none",
+        nicknames: ['Architect']
+    }, {
+        clickCount: 0,
+        name: "Twins",
+        imgSrc: "img/cat_3.jpg",
+        imgAttribution: "none",
+        nicknames: ['Killers']
+    }, {
+        clickCount: 0,
+        name: "Ball",
+        imgSrc: "img/cat_4.jpg",
+        imgAttribution: "none",
+        nicknames: ['Papper']
+    }, {
+        clickCount: 0,
+        name: "Phillip",
+        imgSrc: "img/cat_5.jpg",
+        imgAttribution: "none",
+        nicknames: ['Phil']
+    }, {
+        clickCount: 0,
+        name: "Max",
+        imgSrc: "img/cat_6.jpg",
+        imgAttribution: "none",
+        nicknames: ['ZZzzzzzZZZzzz']
+    }];
+
+var Cat = function (data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nickNames = ko.observableArray(data.nicknames);
 
     this.title = ko.pureComputed(function () {
         var title;
@@ -30,9 +69,17 @@ var Cat = function () {
 };
 
 var ViewModel = function () {
-    this.currentCat = ko.observable(new Cat());
+    var self = this;
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function (catItem) {
+        self.catList.push(new Cat(catItem))
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
+
     this.incrementCounter = function () {
-        this.clickCount(this.clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
 };
 
